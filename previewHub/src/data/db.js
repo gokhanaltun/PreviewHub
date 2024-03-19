f4f1100d958e7e94641f5060e93a1124849f6065
Gökhan Altun <86836411+gokhanaltun@users.noreply.github.com>
2024-03-19 16:38:19 +0300

    edit rooturl name

--- previewHub/src/data/db.js
+++ previewHub/src/data/db.js
@@ -1,21 +1,21 @@
-const rooturl = "https://gokhanaltun.github.io/PreviewHub"
+const rootUrl = "https://gokhanaltun.github.io/PreviewHub"
 export const projects = [
     {
         id: 0,
         title: "Vue3 Password Generator",
         description: "Vue3 Random Password Generator Example",
-        previewUrl: `${rooturl}/vue3-password-generator`
+        previewUrl: `${rootUrl}/vue3-password-generator`
     },
     {
         id: 1,
         title: "Vue3 OCR App",
         description: "Vue3 OCR App Example (cropper.js, tesseract.js)",
-        previewUrl: `${rooturl}/vue3-ocr-app`
+        previewUrl: `${rootUrl}/vue3-ocr-app`
     },
     {
         id: 2,
         title: "Vue3 Shopping Cart",
         description: "Vue3 Simple Shopping Cart Example",
-        previewUrl: `${rooturl}/vue3-shopping-cart`
+        previewUrl: `${rootUrl}/vue3-shopping-cart`
     }
 ]
